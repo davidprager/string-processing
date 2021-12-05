2c1ec5166909d617ea2c0035a622993b138d3ea7
davidprager <david_prager@yahoo.com>
2021-12-04 20:21:19 -0500

    Initial commit

---
+++ strings.js
@@ -0,0 +1,68 @@
+/* This file contains functions to manipulate strings */
+"use strict"
+
+function stringFun() {
+    let str = "To be, or not to be, that is the question:" +
+        "Whether 'tis nobler in the mind to suffer" +
+        "The slings and arrows of outrageous fortune";
+    console.log(str);
+    console.log(" ");
+
+    // Display how long a string is
+/*    console.log("The string has " + getStringLength(str) + " characters");
+    console.log("");*/
+
+//    // Find characters in specific positions
+/*    console.log("The 5th to the 15th characters are ");
+    console.log(getSomeCharacters(str, 5, 15));
+    console.log("")*/
+
+//    // Display the character positions and the characters in them
+/*    console.log("Display positions and characters")
+    displayCharacters(str, 20);
+    console.log('');*/
+
+//    // Find the starting position of a string
+/*    let searchStr = "to"
+    console.log("The starting position of " + searchStr + " is " + findStart(str, searchStr));
+    console.log("");*/
+
+//    // Find all of the starting positions of a string
+/*    console.log("All of the starting positions of " + searchStr + " are:");
+    findAllStarts(str, searchStr);
+    console.log("");*/
+
+//    // Reverse the order of all the characters in a string
+//    reverseString(str);
+}
+
+
+/* This function returns the length of a string */
+function getStringLength(str) {
+
+}
+
+/* This function returns characters in a specifed part of a string */
+function getSomeCharacters(str, n, m) {
+
+}
+
+/* This function displays character positions and the characters in them */
+function displayCharacters(str, n) {
+
+}
+
+/* This function returns the first starting position of a substring */
+function findStart(str, sub) {
+
+}
+
+/* This function displays all the starting positions of a substring */
+function findAllStarts(str, sub) {
+
+}
+
+/* This function reverses a string */
+function reverseString(str) {
+
+}
